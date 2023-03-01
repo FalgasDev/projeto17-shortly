f2238da8710e1e46023bca336e8769af15d0e018
FabioGarroteDev <fgarrote12@gmail.com>
2023-03-01 14:14:35 -0300

    Adicionada rota que mostra os links encurtados do usuário

--- src/app.js
+++ src/app.js
@@ -3,13 +3,14 @@ import cors from "cors"
 import dotenv from "dotenv"
 import authRouter from "./routes/authRoutes.js"
 import urlRouter from "./routes/urlsRoutes.js"
+import userRouter from "./routes/usersRoutes.js"
 
 dotenv.config()
 
 const server = express()
 server.use(cors())
 server.use(express.json())
-server.use([authRouter, urlRouter])
+server.use([authRouter, urlRouter, userRouter])
 
 const port = process.env.PORT || 5000
 
