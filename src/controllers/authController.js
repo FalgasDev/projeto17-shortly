0908b2c1edd3fa2a481abe7bd95c53d1bf2e814b
FabioGarroteDev <fgarrote12@gmail.com>
2023-03-01 14:41:39 -0300

    Fix da rota signIn que antes retornava só uma string e agora retorna um objeto

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -40,7 +40,7 @@ export async function signIn(req, res) {
       await db.query('INSERT INTO sessions (token, "userId") VALUES ($1, $2)', [token, user.rows[0].id])
     }
 
-    res.send(token)
+    res.send({token: token})
   } catch (err) {
     res.status(500).send(err.message)
   }
